8f49f2083ec31e225ddd0e0ba17baae9ee943cc6
JustFavour <odeyemifavour1@gmail.com>
2025-01-18 22:09:37 +0100

    added hardhat for seemless verification on new chains

---
+++ hardhat.config.js
@@ -0,0 +1,91 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-foundry");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.27",
+
+  networks: {
+    sonicTestnet: {
+      url: "https://rpc.blaze.soniclabs.com",
+      chainId: 57054,
+      accounts: [process.env.private_key], 
+    },
+    kairos: {
+      url: "https://public-en-kairos.node.kaia.io",
+      chainId: 1001,
+      accounts: [process.env.private_key], 
+    },
+    basesepolia: {
+      url: process.env.BASE_RPC_URL,
+      chainId: 11155111, 
+      accounts: [process.env.private_key], 
+      gasPrice: 1000000000,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      chainId: 11155111,
+      accounts: [process.env.private_key],
+    },
+    lisksepolia: {
+      url: process.env.Lisk_SEPOLIA_RPC_URL,
+      chainId: 4202, 
+      accounts: [process.env.private_key],
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      basesepolia: process.env.BASESCAN_API_KEY,
+      kairos: "unnecessary", 
+      sonicTestnet: "YOUR_SONICSCAN_TESTNET_API_KEY",
+      sepolia: process.env.SEPOLIA_API_KEY,
+      lisksepolia: process.env.LISK_API_KEY,
+    },
+    customChains: [
+      
+      {
+        network: "sonicTestnet",
+        chainId: 57054,
+        urls: {
+          apiURL: "https://api-testnet.sonicscan.org/api",
+          browserURL: "https://testnet.sonicscan.org",
+        },
+      },
+      {
+        network: "kairos",
+        chainId: 1001,
+        urls: {
+          apiURL: "https://api-baobab.klaytnscope.com/api",
+          browserURL: "https://kairos.kaiascope.com",
+        },
+      },
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://api-sepolia.etherscan.io/api",
+          browserURL: "https://sepolia.etherscan.io",
+        },
+      },
+      {
+        network: "lisksepolia",
+        chainId: 4202, 
+         urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      },
+    ],
+  },
+  ignition: {
+    strategyConfig: {
+      create2: {
+        // To learn more about salts, see the CreateX documentation
+        // bytes32  SALT_AIRDROP = keccak256("SonikDropAirdropFactoryFacet");
+        salt: "0xfb4dfcab774cb21c02420d33a6f90c9edcdd51187d92fa7f029d3e26af184b49",
+      },
+    },
+  },
+};
---
+++ iginition/modules/deploy.js
@@ -0,0 +1,13 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const DeployModule = buildModule("DeployModule", (m) => {
+    // Deploy AirdropFactoryFacet
+    const airdropFactoryFacet = m.contract("AirdropFactoryFacet");
+
+    // Deploy PoapFactoryFacet
+    const poapFactoryFacet = m.contract("PoapFactoryFacet");
+
+    return { airdropFactoryFacet, poapFactoryFacet };
+});
+
+export default DeployModule;
